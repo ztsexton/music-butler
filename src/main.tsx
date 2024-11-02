4e96a3baf68b839e1e76a7580cec7a862a535569
Zach Sexton <zsexton2011@gmail.com>
2024-11-02 02:52:09 -0400

    Push

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // Import BrowserRouter
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
